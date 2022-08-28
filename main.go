f0446c2c8571c0d407c5eb559a51d4436f373585
Jonathan Moura <jonathanmourax4@gmail.com>
2022-08-28 13:16:21 -0300

    test

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
+
+	data, err := os.ReadFile("files/test.txt")
+	check(err)
+
+	fmt.Println(string(data))
 
 }
